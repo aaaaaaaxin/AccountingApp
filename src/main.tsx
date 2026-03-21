6b7377656cf856a142de49a19d170bdb62ae7865
lhonour <956124843@qq.com>
2026-03-21 16:45:47 +0800

    fix: disable service worker to avoid fetch failures

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,6 @@ import App from './App'
 import './index.css'
 import { AppDialogProvider } from './components/common/AppDialogProvider'
 
-if (import.meta.env.PROD && 'serviceWorker' in navigator && !window.location.hostname.endsWith('.pages.dev')) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-  })
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppDialogProvider>
